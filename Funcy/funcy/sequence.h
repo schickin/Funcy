6461bc1d7626894d205374cf1f74573d0e371324
Thomas Schickinger <combean@sourceforge.net>
2013-06-21 22:17:22 +0200

    - copy constructor and assignment for untyped Sequence added

--- Funcy/funcy/sequence.h
+++ Funcy/funcy/sequence.h
@@ -10,45 +10,95 @@
 
 #include "funcy/impl/sequence_crtp.h"
 
-
-class Sequence : public SequenceCRTP<Sequence>
+template <typename ElemType>
+class Sequence : public SequenceCRTP<Sequence<ElemType> >
 {
 public:
+  typedef ElemType Elem;
 
   Sequence() :
+    innerSeqPtr_(nullptr),
     emptyPtr_(nullptr),
     cvalPtr_(nullptr),
     nextPtr_(nullptr)
   { }
 
+  // default copy constructor
+
+  // constructor from any other sequence
+  template <typename SequenceImpl>
+  Sequence(SequenceImpl& rSeq) :
+    innerSeqPtr_(&rSeq),
+    emptyPtr_(emptyImpl<SequenceImpl>),
+    cvalPtr_(cvalImpl<SequenceImpl>),
+    nextPtr_(nextImpl<SequenceImpl>)
+  { }
+
+  // default assignment operator
+
+  // assignment from any other sequence
+  template <typename SequenceImpl>
+  Sequence& operator=(SequenceImpl& rSeq)
+  {
+    innerSeqPtr_ = &rSeq;
+    emptyPtr_ = emptyImpl<SequenceImpl>;
+    cvalPtr_ = cvalImpl<SequenceImpl>;
+    nextPtr_ = nextImpl<SequenceImpl>;
+
+    return *this;
+  }
 
   bool empty() const
   {
     assert(emptyPtr_);
-    return emptyPtr_();
+    return emptyPtr_(*this);
   }
 
-  const Elem cval() const
+  Elem cval() const
   {
     assert(cvalPtr_);
-    return cvalPtr_();
+    return cvalPtr_(*this);
   }
 
   void next()
   {
     assert(nextPtr_);
-    return nextPtr_();
+    return nextPtr_(*this);
+  }
+
+  bool initialized() const
+  {
+    return innerSeqPtr_ != nullptr;
   }
 
 private:
 
-  typedef bool (*EmptyFuncPtr)();
-  typedef const Elem (*CValFuncPtr)();
-  typedef void (*NextFuncPtr)();
+  typedef bool (*EmptyFuncPtr)(const Sequence&);
+  typedef Elem (*CValFuncPtr)(const Sequence&);
+  typedef void (*NextFuncPtr)(Sequence&);
 
+  void* innerSeqPtr_;
   EmptyFuncPtr emptyPtr_;
   CValFuncPtr cvalPtr_;
   NextFuncPtr nextPtr_;
+
+  template <typename SequenceImpl>
+  static bool emptyImpl(const Sequence& rSeq)
+  {
+    return reinterpret_cast<SequenceImpl*>(rSeq.innerSeqPtr_)->empty();
+  }
+
+  template <typename SequenceImpl>
+  static Elem cvalImpl(const Sequence& rSeq)
+  {
+    return reinterpret_cast<SequenceImpl*>(rSeq.innerSeqPtr_)->cval();
+  }
+
+  template <typename SequenceImpl>
+  static void nextImpl(Sequence& rSeq)
+  {
+    return reinterpret_cast<SequenceImpl*>(rSeq.innerSeqPtr_)->next();
+  }
 
 };
 
--- Funcy/tst/UntypedSequenceTest.cpp
+++ Funcy/tst/UntypedSequenceTest.cpp
@@ -5,7 +5,7 @@
  *      Author: schickin
  */
 
-
+#include "funcy/iterator_seq.h"
 #include "funcy/sequence.h"
 
 #include "gtest/gtest.h"
@@ -13,11 +13,75 @@
 
 class UntypedSequenceTest : public ::testing::Test
 {
+protected:
 
+  template <typename ElemType>
+  void checkOneElementSequence(const ElemType& expected, Sequence<ElemType>& seq)
+  {
+    EXPECT_TRUE(seq.initialized());
+
+    EXPECT_FALSE(seq.empty());
+    EXPECT_EQ(expected, seq.cval());
+    seq.next();
+    EXPECT_TRUE(seq.empty());
+  }
 };
 
-TEST_F(UntypedSequenceTest, DISABLED_createWithoutInitialization)
+TEST_F(UntypedSequenceTest, createWithoutInitialization)
 {
-  Sequence seq;
-  EXPECT_FALSE(seq);
+  Sequence<int> seq;
+  EXPECT_FALSE(seq.initialized());
+};
+
+TEST_F(UntypedSequenceTest, createFromOtherSequence)
+{
+  auto otherSeq = make_seq({1});
+
+  Sequence<int> seq(otherSeq);
+
+  checkOneElementSequence(1, seq);
+};
+
+TEST_F(UntypedSequenceTest, createFromOtherSequenceWithElemTypeConversion)
+{
+  auto otherSeq = make_seq({2.5});
+
+  Sequence<int> seq(otherSeq);
+
+  checkOneElementSequence(2, seq);
+};
+
+TEST_F(UntypedSequenceTest, defaultCopyConstructor)
+{
+  auto otherSeq = make_seq({1});
+
+  Sequence<int> seq(otherSeq);
+  Sequence<int> copiedSeq(seq);
+
+  checkOneElementSequence(1, copiedSeq);
+};
+
+TEST_F(UntypedSequenceTest, defaultAssignmentOperator)
+{
+  auto otherSeq = make_seq({'a'});
+
+  Sequence<char> seq(otherSeq);
+  Sequence<char> copiedSeq;
+  EXPECT_FALSE(copiedSeq.initialized());
+
+  copiedSeq = seq;
+
+  checkOneElementSequence('a', copiedSeq);
+};
+
+TEST_F(UntypedSequenceTest, assignmentFromOtherSequence)
+{
+  auto otherSeq = make_seq({10});
+
+  Sequence<int> seq;
+  EXPECT_FALSE(seq.initialized());
+
+  seq = otherSeq;
+
+  checkOneElementSequence(10, seq);
 };
