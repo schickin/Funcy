2dd72fcc04426b72da11778bccb1c901b83e1abb
Thomas Schickinger <combean@sourceforge.net>
2013-06-22 22:46:21 +0200

    Value semantics for untyped sequences added (copy constructors and assignment operators for all kinds of sequences).

--- Funcy/funcy/sequence.h
+++ Funcy/funcy/sequence.h
@@ -20,30 +20,107 @@ public:
     innerSeqPtr_(nullptr),
     emptyPtr_(nullptr),
     cvalPtr_(nullptr),
-    nextPtr_(nullptr)
+    nextPtr_(nullptr),
+    deletePtr_(nullptr),
+    clonePtr_(nullptr)
   { }
 
-  // default copy constructor
-
-  // constructor from any other sequence
-  template <typename SequenceImpl>
-  Sequence(SequenceImpl& rSeq) :
-    innerSeqPtr_(&rSeq),
-    emptyPtr_(emptyImpl<SequenceImpl>),
-    cvalPtr_(cvalImpl<SequenceImpl>),
-    nextPtr_(nextImpl<SequenceImpl>)
+  Sequence(const Sequence& other) :
+    innerSeqPtr_(other.clonePtr_(other)),
+    emptyPtr_(other.emptyPtr_),
+    cvalPtr_(other.cvalPtr_),
+    nextPtr_(other.nextPtr_),
+    deletePtr_(other.deletePtr_),
+    clonePtr_(other.clonePtr_)
   { }
 
-  // default assignment operator
+  // constructor from any other untyped sequence
+  template <typename OtherElemType>
+  Sequence(const Sequence<OtherElemType>& other) :
+    innerSeqPtr_(other.clonePtr_(other)),
+    emptyPtr_(other.emptyPtr_),
+    cvalPtr_(other.cvalPtr_),
+    nextPtr_(other.nextPtr_),
+    deletePtr_(other.deletePtr_),
+    clonePtr_(other.clonePtr_)
+  { }
 
-  // assignment from any other sequence
+  // constructor from any other typed sequence
   template <typename SequenceImpl>
-  Sequence& operator=(SequenceImpl& rSeq)
+  Sequence(const SequenceImpl& rSeq) :
+    innerSeqPtr_(new SequenceImpl(rSeq)),
+    emptyPtr_(&emptyImpl<SequenceImpl>),
+    cvalPtr_(&cvalImpl<SequenceImpl>),
+    nextPtr_(&nextImpl<SequenceImpl>),
+    deletePtr_(&deleteImpl<SequenceImpl>),
+    clonePtr_(&cloneImpl<SequenceImpl>)
+  { }
+
+  ~Sequence()
   {
-    innerSeqPtr_ = &rSeq;
-    emptyPtr_ = emptyImpl<SequenceImpl>;
-    cvalPtr_ = cvalImpl<SequenceImpl>;
-    nextPtr_ = nextImpl<SequenceImpl>;
+    if (deletePtr_) {
+        deletePtr_(*this);
+    }
+  }
+
+  Sequence& operator=(const Sequence& other)
+  {
+    if (this == &other) {
+        return *this;
+    }
+
+    void* pSeq = other.clonePtr_(other);
+
+    if (deletePtr_) {
+        deletePtr_(*this);
+    }
+
+    innerSeqPtr_ = pSeq;
+    emptyPtr_ = other.emptyPtr_;
+    cvalPtr_ = other.cvalPtr_;
+    nextPtr_ = other.nextPtr_;
+    deletePtr_ = other.deletePtr_;
+    clonePtr_ = other.clonePtr_;
+
+    return *this;
+  }
+
+  // assignment from any other untyped sequence
+  template <typename OtherElemType>
+  Sequence& operator=(const Sequence<OtherElemType>& other)
+  {
+    void* pSeq = other.clonePtr_(other);
+
+    if (deletePtr_) {
+        deletePtr_(*this);
+    }
+
+    innerSeqPtr_ = pSeq;
+    emptyPtr_ = other.emptyPtr_;
+    cvalPtr_ = other.cvalPtr_;
+    nextPtr_ = other.nextPtr_;
+    deletePtr_ = other.deletePtr_;
+    clonePtr_ = other.clonePtr_;
+
+    return *this;
+  }
+
+  // assignment from any other typed sequence
+  template <typename SequenceImpl>
+  Sequence& operator=(const SequenceImpl& rSeq)
+  {
+    SequenceImpl* pSeq = new SequenceImpl(rSeq);
+
+    if (deletePtr_) {
+        deletePtr_(*this);
+    }
+
+    innerSeqPtr_ = pSeq;
+    emptyPtr_ = &emptyImpl<SequenceImpl>;
+    cvalPtr_ = &cvalImpl<SequenceImpl>;
+    nextPtr_ = &nextImpl<SequenceImpl>;
+    deletePtr_ = &deleteImpl<SequenceImpl>;
+    clonePtr_ = &cloneImpl<SequenceImpl>;
 
     return *this;
   }
@@ -76,11 +153,15 @@ private:
   typedef bool (*EmptyFuncPtr)(const Sequence&);
   typedef Elem (*CValFuncPtr)(const Sequence&);
   typedef void (*NextFuncPtr)(Sequence&);
+  typedef void (*DeleteFuncPtr)(Sequence&);
+  typedef void* (*CloneFuncPtr)(const Sequence&);
 
   void* innerSeqPtr_;
   EmptyFuncPtr emptyPtr_;
   CValFuncPtr cvalPtr_;
   NextFuncPtr nextPtr_;
+  DeleteFuncPtr deletePtr_;
+  CloneFuncPtr clonePtr_;
 
   template <typename SequenceImpl>
   static bool emptyImpl(const Sequence& rSeq)
@@ -98,6 +179,23 @@ private:
   static void nextImpl(Sequence& rSeq)
   {
     return reinterpret_cast<SequenceImpl*>(rSeq.innerSeqPtr_)->next();
+  }
+
+  template <typename SequenceImpl>
+  static void deleteImpl(Sequence& rSeq)
+  {
+    delete reinterpret_cast<SequenceImpl*>(rSeq.innerSeqPtr_);
+  }
+
+  template <typename SequenceImpl>
+  static void* cloneImpl(const Sequence& rSeq)
+  {
+    if (rSeq.innerSeqPtr_) {
+      return new SequenceImpl(*reinterpret_cast<const SequenceImpl*>(rSeq.innerSeqPtr_));
+    }
+    else {
+      return nullptr;
+    }
   }
 
 };
